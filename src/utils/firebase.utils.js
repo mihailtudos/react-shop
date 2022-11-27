6e7f0387ed997b8f18ffb972b3224d52f3be6135
Mihail Tudos <mihail.tudos9@gmail.com>
2022-11-27 14:11:21 +0000

    added firebase utils to store users in the firebase db

--- src/utils/firebase.utils.js
+++ src/utils/firebase.utils.js
@@ -1,8 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import {getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import {
+	getFirestore,
+	doc,
+	getDoc,
+	setDoc
+} from 'firebase/firestore'
 // Your web app's Firebase configuration
 const firebaseConfig = {
 	apiKey: "AIzaSyAwxXfGzMKAnZXoPcMcCcU5ZBXI7eMzJKk",
@@ -24,3 +31,29 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+	const userDocRef = doc(db, 'users',userAuth.uid);
+	const userSnapshot = await getDoc(userDocRef);
+
+	//if user data does not exists
+	if (!userSnapshot.exists()) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await setDoc(userDocRef, {
+				displayName,
+				email,
+				createdAt
+			})
+		} catch (error) {
+			console.log("There was an error creating the user", error.message)
+		}
+	}
+
+	return userDocRef;
+}
