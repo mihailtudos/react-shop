2e691dd73298cd018a3457ccca200e640cebd8ed
Mihail Tudos <mihail.tudos9@gmail.com>
2022-12-05 19:23:31 +0000

    fixed merging issues

--- src/index.js
+++ src/index.js
@@ -1,42 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import {Provider} from "react-redux";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
-<<<<<<< HEAD
-import {UserProvider} from "./context/user.context";
-import {CategoriesProvider} from "./context/categories.context";
-import {CartProvider} from "./context/cart.context";
-import {store} from "./store/store";
-
-=======
 import {Provider} from "react-redux";
 import {persistor, store} from "./store/store";
 import {PersistGate} from "redux-persist/integration/react";
->>>>>>> origin/redux
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
-	  <Provider store={store}>
-		  <BrowserRouter>
-				  <CategoriesProvider>
-					  <CartProvider>
-						  <App />
-					  </CartProvider>
-				  </CategoriesProvider>
-		  </BrowserRouter>
-=======
 	  <Provider store={store} >
 		  <PersistGate loading={null} persistor={persistor}>
 			  <BrowserRouter>
 				  <App />
 			  </BrowserRouter>
 		  </PersistGate>
->>>>>>> origin/redux
 	  </Provider>
   </React.StrictMode>
 );
