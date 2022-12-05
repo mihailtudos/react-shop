6966d6b77b175389b3ec9aa55dad007a93d7cdc7
Mihail Tudos <mihail.tudos9@gmail.com>
2022-12-05 19:08:44 +0000

    added persistent storage for redux storage

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,27 @@
 import {compose, createStore, applyMiddleware} from "redux";
 import {logger} from "redux-logger/src";
 import {rootReducer} from "./root-reducer";
+import storage from 'redux-persist/lib/storage';
+import {persistReducer, persistStore} from "redux-persist";
 
 //creating the list of middleware (a list of libraries that run before an action hits a reducer)
-const middleware = [logger];
+const middleware = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 //applying middleware
-const composedEnhancers = compose(applyMiddleware(...middleware));
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleware));
+
+const persistConfig = {
+	key: 'root',
+	storage,
+	blacklist: ['user']
+};
+
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 //root-reducer (combines the app reducers)
 //the store is intended to facilitate the actions and movement and passing of actions through this reducers
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store);
